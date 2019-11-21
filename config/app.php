81fd5481820725074a9e2ea154c2b6adb5b870a9
Daniel Rodriguez <dannyjose1112@hotmail.com>
2019-11-21 11:38:04 -0600

    [UPDATE] added 2checkout

--- config/app.php
+++ config/app.php
@@ -164,7 +164,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\Cors\ServiceProvider::class,
-
+        TwoCheckout\TwoCheckoutServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -226,6 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'TwoCheckout' => TwoCheckout\Facades\TwoCheckout::class
 
     ],
 
