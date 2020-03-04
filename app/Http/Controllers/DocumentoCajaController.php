97af7b7f46e3ae050cfdbf7ba3a8ff5cd27dd80b
Daniel Rodriguez <dannyjose1112@hotmail.com>
2020-03-03 23:16:25 -0600

    [UPDATED] se agregaron los controladores base de caja

---
+++ app/Http/Controllers/DocumentoCajaController.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\DocumentoCaja;
+use Response;
+use Validator;
+class DocumentoCajaController extends Controller
+{
+    /**
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function index()
+    {
+        return Response::json(DocumentoCaja::all(), 200);
+    }
+    
+    public function getThisByFilter(Request $request, $id,$state)
+    {
+        if($request->get('filter')){
+            switch ($request->get('filter')) {
+                case 'state':{
+                    $objectSee = DocumentoCaja::whereRaw('user=? and state=?',[$id,$state])->with('user')->get();
+                    break;
+                }
+                case 'type':{
+                    $objectSee = DocumentoCaja::whereRaw('user=? and tipo=?',[$id,$state])->with('user')->get();
+                    break;
+                }
+                default:{
+                    $objectSee = DocumentoCaja::whereRaw('user=? and state=?',[$id,$state])->with('user')->get();
+                    break;
+                }
+    
+            }
+        }else{
+            $objectSee = DocumentoCaja::whereRaw('user=? and state=?',[$id,$state])->with('user')->get();
+        }
+    
+        if ($objectSee) {
+            return Response::json($objectSee, 200);
+    
+        }
+        else {
+            $returnData = array (
+                'status' => 404,
+                'message' => 'No record found'
+            );
+            return Response::json($returnData, 404);
+        }
+    }
+    
+    /**
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function create()
+    {
+        //
+    }
+    
+    /**
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            ''          => 'required',
+            ''          => 'required',
+        ]);
+        if ( $validator->fails() ) {
+            $returnData = array (
+                'status' => 400,
+                'message' => 'Invalid Parameters',
+                'validator' => $validator
+            );
+            return Response::json($returnData, 400);
+        }
+        else {
+            try {
+                $newObject = new DocumentoCaja();
+                $newObject->column            = $request->get('column');
+                $newObject->save();
+                return Response::json($newObject, 200);
+    
+            } catch (Exception $e) {
+                $returnData = array (
+                    'status' => 500,
+                    'message' => $e->getMessage()
+                );
+                return Response::json($returnData, 500);
+            }
+        }
+    }
+    
+    /**
+    * Display the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function show($id)
+    {
+        $objectSee = DocumentoCaja::find($id);
+        if ($objectSee) {
+            $objectSee->column;
+            return Response::json($objectSee, 200);
+    
+        }
+        else {
+            $returnData = array (
+                'status' => 404,
+                'message' => 'No record found'
+            );
+            return Response::json($returnData, 404);
+        }
+    }
+    
+    /**
+    * Show the form for editing the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function edit($id)
+    {
+        //
+    }
+    
+    /**
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function update(Request $request, $id)
+    {
+        $objectUpdate = DocumentoCaja::find($id);
+        if ($objectUpdate) {
+            try {
+                $objectUpdate->column = $request->get('column', $objectUpdate->column);
+    
+                $objectUpdate->save();
+                $objectUpdate->function;
+                return Response::json($objectUpdate, 200);
+            } catch (Exception $e) {
+                $returnData = array (
+                    'status' => 500,
+                    'message' => $e->getMessage()
+                );
+                return Response::json($returnData, 500);
+            }
+        }
+        else {
+            $returnData = array (
+                'status' => 404,
+                'message' => 'No record found'
+            );
+            return Response::json($returnData, 404);
+        }
+    }
+    
+    
+    /**
+    * Remove the specified resource from storage.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function destroy($id)
+    {
+        $objectDelete = DocumentoCaja::find($id);
+        if ($objectDelete) {
+            try {
+                DocumentoCaja::destroy($id);
+                return Response::json($objectDelete, 200);
+            } catch (Exception $e) {
+                $returnData = array (
+                    'status' => 500,
+                    'message' => $e->getMessage()
+                );
+                return Response::json($returnData, 500);
+            }
+        }
+        else {
+            $returnData = array (
+                'status' => 404,
+                'message' => 'No record found'
+            );
+            return Response::json($returnData, 404);
+        }
+    }
+}
---
+++ app/Http/Controllers/PolizasController.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Polizas;
+use Response;
+use Validator;
+class PolizasController extends Controller
+{
+    /**
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function index()
+    {
+        return Response::json(Polizas::all(), 200);
+    }
+    
+    public function getThisByFilter(Request $request, $id,$state)
+    {
+        if($request->get('filter')){
+            switch ($request->get('filter')) {
+                case 'state':{
+                    $objectSee = Polizas::whereRaw('user=? and state=?',[$id,$state])->with('user')->get();
+                    break;
+                }
+                case 'type':{
+                    $objectSee = Polizas::whereRaw('user=? and tipo=?',[$id,$state])->with('user')->get();
+                    break;
+                }
+                default:{
+                    $objectSee = Polizas::whereRaw('user=? and state=?',[$id,$state])->with('user')->get();
+                    break;
+                }
+    
+            }
+        }else{
+            $objectSee = Polizas::whereRaw('user=? and state=?',[$id,$state])->with('user')->get();
+        }
+    
+        if ($objectSee) {
+            return Response::json($objectSee, 200);
+    
+        }
+        else {
+            $returnData = array (
+                'status' => 404,
+                'message' => 'No record found'
+            );
+            return Response::json($returnData, 404);
+        }
+    }
+    /**
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function create()
+    {
+        //
+    }
+    
+    /**
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            ''          => 'required',
+            ''          => 'required',
+        ]);
+        if ( $validator->fails() ) {
+            $returnData = array (
+                'status' => 400,
+                'message' => 'Invalid Parameters',
+                'validator' => $validator
+            );
+            return Response::json($returnData, 400);
+        }
+        else {
+            try {
+                $newObject = new Polizas();
+                $newObject->column            = $request->get('column');
+                $newObject->save();
+                return Response::json($newObject, 200);
+    
+            } catch (Exception $e) {
+                $returnData = array (
+                    'status' => 500,
+                    'message' => $e->getMessage()
+                );
+                return Response::json($returnData, 500);
+            }
+        }
+    }
+    
+    /**
+    * Display the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function show($id)
+    {
+        $objectSee = Polizas::find($id);
+        if ($objectSee) {
+            $objectSee->column;
+            return Response::json($objectSee, 200);
+    
+        }
+        else {
+            $returnData = array (
+                'status' => 404,
+                'message' => 'No record found'
+            );
+            return Response::json($returnData, 404);
+        }
+    }
+    
+    /**
+    * Show the form for editing the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function edit($id)
+    {
+        //
+    }
+    
+    /**
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function update(Request $request, $id)
+    {
+        $objectUpdate = Polizas::find($id);
+        if ($objectUpdate) {
+            try {
+                $objectUpdate->column = $request->get('column', $objectUpdate->column);
+    
+                $objectUpdate->save();
+                $objectUpdate->function;
+                return Response::json($objectUpdate, 200);
+            } catch (Exception $e) {
+                $returnData = array (
+                    'status' => 500,
+                    'message' => $e->getMessage()
+                );
+                return Response::json($returnData, 500);
+            }
+        }
+        else {
+            $returnData = array (
+                'status' => 404,
+                'message' => 'No record found'
+            );
+            return Response::json($returnData, 404);
+        }
+    }
+    
+    
+    /**
+    * Remove the specified resource from storage.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function destroy($id)
+    {
+        $objectDelete = Polizas::find($id);
+        if ($objectDelete) {
+            try {
+                Polizas::destroy($id);
+                return Response::json($objectDelete, 200);
+            } catch (Exception $e) {
+                $returnData = array (
+                    'status' => 500,
+                    'message' => $e->getMessage()
+                );
+                return Response::json($returnData, 500);
+            }
+        }
+        else {
+            $returnData = array (
+                'status' => 404,
+                'message' => 'No record found'
+            );
+            return Response::json($returnData, 404);
+        }
+    }
+}
